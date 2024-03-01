f6d6bedcdec988ff1421fb756ed452b5ec9e6e4d
MuhammadFahadgithub <159857192+MuhammadFahadgithub@users.noreply.github.com>
2024-03-01 17:20:50 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,50 @@
+// Task 23
+// Write a series of conditional tests.
+// creating car variable
+
+let car :string= 'subaru';
+// Test No. 1
+console.log("Is car === 'subaru'? I predict True.");
+console.log(car ===`subaru`);
+
+// Test No. 2
+console.log("Is car == 'subaru'? I predict True.");
+console.log(car ==`subaru`);
+
+// Test No. 3
+console.log("Is car != 'Suzuki'? I predict True.");
+console.log(car != 'Suzuki');
+
+// Test No. 4
+console.log("Is car !== 'Swift'? I predict True.");
+console.log(car !== 'Swift');
+
+// Test No. 5
+console.log("Is car.Upper case == 'SUBARU'? I predict True.");
+console.log(car.toUpperCase ()== 'SUBARU');
+
+// Test No. 6
+console.log("Is car.Lower case == 'SUBARU'? I predict True.");
+console.log(car.toLowerCase ()== 'SUBARU');
+
+// Test No. 7
+console.log("Is car === 'SUBARU'? I predict True.");
+console.log(car === 'SUBARU');
+
+// Test No. 8
+console.log("Is car === 'Train'? I predict True.");
+console.log(car === 'Train');
+
+// Test No. 9
+console.log("Is car === 'Bus'? I predict True.");
+console.log(car === 'Bus');
+
+// Test No. 10
+console.log("Is car === 'Cycle'? I predict True.");
+console.log(car === 'Cycle');
+
+// = means set the value of something.
+//  == means check the value of somthing.
+//  === means check with data type of something.
+// != means not equal to
+// example: 2 != 2 false
